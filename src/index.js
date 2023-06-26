31594533d0d3383d183ef8a65f3696e6994bd8e2
Rolando Marzano <rolo@rolos-mbp.lan>
2023-06-26 03:58:44 -0500

    Mapping buttons on frontend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { BigNumber, Contract, providers, ethers, utils } from "ethers";
+import { BigNumber, Contract, providers, ethers, utils } from 'ethers';
 
-// import usdcTknAbi ...
-// import miPrimerTknAbi ...
-// import publicSaleAbi ...
-// import nftTknAbi ...
+import usdcTknAbi from '../artifacts/contracts/USDCoin.sol/USDCoin.json';
+import miPrimerTknAbi from '../artifacts/contracts/MiPrimerToken.sol/MiPrimerToken.json';
+import publicSaleAbi from '../artifacts/contracts/PublicSale.sol/PublicSale.json';
+import nftTknAbi from '../artifacts/contracts/NFT.sol/MiPrimerNft.json';
 
 window.ethers = ethers;
 
@@ -30,11 +30,49 @@ function initSCsGoerli() {
 // Se pueden escuchar eventos de los contratos usando el provider con RPC
 function initSCsMumbai() {
   var nftAddress;
-  nftTknContract; // = new Contract...
+  nftTknContract = new Contract(
+    nftAddress,
+    nftTknAbi.abi,
+    new providers.JsonRpcProvider(url)
+  );
 }
 
 function setUpListeners() {
   // Connect to Metamask
+  var connectBtn = document.getElementById('connect');
+
+  connectBtn.addEventListener('click', async () => {
+    if (window.ethereum) {
+      [account] = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Billetera metamask', account);
+
+      // provider: Metamask, estamos usando window.ethereum
+      provider = new providers.Web3Provider(window.ethereum);
+      // signer: el que va a firmar las tx
+      signer = provider.getSigner(account);
+      window.signer = signer;
+    }
+  });
+
+  var usdcUpdateBtn = document.getElementById('usdcUpdate');
+  usdcUpdateBtn.addEventListener('click', async () => {});
+
+  var miPrimerTknBalanceBtn = document.getElementById('miPrimerTknBalance');
+  miPrimerTknBalanceBtn.addEventListener('click', async () => {});
+
+  var approveButtonBtn = document.getElementById('approveButton');
+  approveButtonBtn.addEventListener('click', async () => {});
+
+  var purchaseButtonBtn = document.getElementById('purchaseButton');
+  purchaseButtonBtn.addEventListener('click', async () => {});
+
+  var purchaseEthButtonBtn = document.getElementById('purchaseEthButton');
+  purchaseEthButtonBtn.addEventListener('click', async () => {});
+
+  var sendEtherButtonBtn = document.getElementById('sendEtherButton');
+  sendEtherButtonBtn.addEventListener('click', async () => {});
 }
 
 function setUpEventsContracts() {
