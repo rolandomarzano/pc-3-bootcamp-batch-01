cd395aa357e6b70820d19e97892eb422d52bc9e7
Rolando Marzano <rolo@Rolos-MacBook-Pro.local>
2023-06-26 18:10:47 -0500

    Update deployGoerli.js

--- scripts/deployGoerli.js
+++ scripts/deployGoerli.js
@@ -10,35 +10,28 @@ const {
 } = require('../utils');
 
 async function deployGoerli() {
-  // Deploy USDC
-  var usdcContract = await deploySCNoUp('USDCoin');
-  var implementationusdcContract = await printAddress(
-    'USDC',
-    usdcContract.address
-  );
-  verify(implementationusdcContract, 'USDC');
-
-  // Deploy MiPrimerToken
-  var miPrimerToken = await deploySC('MiPrimerToken', []);
-  var implementationMiPrimerToken = await printAddress(
-    'MiPrimerToken',
-    miPrimerToken.address
-  );
-  verify(implementationMiPrimerToken, 'MiPrimerToken', []);
-
-  // Deploy PublicSale
   var gnosis = { address: '0x655252000B5aC35239C9B7F112d3F252874763f4' };
 
-  var publicSale = await deploySC('PublicSale', []);
+  var publicSale = await deploySC('PublicSale');
+  var miPrimerToken = await deploySC('MiPrimerToken');
+  var usdcContract = await deploySCNoUp('USDCoin');
+
   var implementationPublicSale = await printAddress(
     'PublicSale',
     publicSale.address
+  );
+
+  var implementationMiPrimerToken = await printAddress(
+    'MiPrimerToken',
+    miPrimerToken.address
   );
 
   await ex(publicSale, 'setMiPrimerToken', [miPrimerToken.address], 'SMP');
   await ex(publicSale, 'setGnosisWallet', [gnosis.address], 'SGW');
 
   verify(implementationPublicSale, 'PublicSale', []);
+  verify(implementationMiPrimerToken, 'MiPrimerToken', []);
+  verify(usdcContract.address, 'USDC');
 }
 
 deployGoerli().catch((error) => {
